012126d7398566b88f9a8274b8f06990d22fad20
Pre-even1972 <kasalikaprincestungz@gmail.com>
2024-11-13 09:17:24 +0200

    adding homepage

--- patient-reminder/src/Components/home/Home.js
+++ patient-reminder/src/Components/home/Home.js
@@ -1,11 +1,40 @@
 import React from 'react';
-const Home = () => {
-    
-    return (
-        <div className="flex items-center justify-center h-screen bg-black text-white px-5 ml-10">
-            <h1 className="text-4xl font-bold">Whatsapp group 10 member are you excited with me code me please</h1>
-        </div>
-    );
-};
+import img2 from './images/project.png';
+import img1 from './images/confi.jpg';
+import img3 from './images/sponsor.jpg';
+import './Home.css';
 
-export default Home;
+function Images() {
+  return (
+    <div className="image-container">
+      <img src={img2} alt="Project" className="logo"/>
+      <div className="nav-container">
+        <a href="#">Home</a>
+        <a href="#">About us</a>
+      </div>
+      <img src={img3} alt="Sponsor" className="handshake"/>
+      <h2>Welcome To Zitheka</h2>
+      <h1>"Apatsa" Healthy care <img src={img1} alt="Confi" className="stethoscope"/></h1>
+      {/* Inline styling for testing */}
+      <a 
+        href="#" 
+        className="sign-in-button" 
+        style={{
+          display: 'inline-block', 
+          padding: '12px 30px', 
+          backgroundColor: '#002366', 
+          color: 'white', 
+          borderRadius: '5px', 
+          fontSize: '18px', 
+          fontWeight: 'bold', 
+          textDecoration: 'none', 
+          marginTop: '20px'
+        }}
+      >
+        Sign In
+      </a>
+    </div>
+  );
+}
+
+export default Images;
